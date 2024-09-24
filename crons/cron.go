4a81a984e890543d52e8dbbeb974f22b1d589ed2
Muntadhar Abbas[alias] <muntadhar.abbas@baly.iq>
2024-09-25 02:36:00 +0300

    checking the server time.now()

--- crons/cron.go
+++ crons/cron.go
@@ -9,6 +9,7 @@ import (
 )
 
 func InitCron() {
+	fmt.Println("hey", time.Now())
 	every12("02:30AM", func() { fmt.Println("hey") })    // 8:15 AM Baghdad time
 	every12("08:15AM", func() { bot.Act("MentionAll") }) // 8:15 AM Baghdad time
 	every12("08:20AM", func() { bot.Act("bestStreak") }) // 8:20 AM Baghdad time
@@ -23,7 +24,7 @@ func every12(twelveHourTime string, job func()) {
 		return
 	}
 
-	baghdadTime := t.Add(3 * time.Hour) 
+	baghdadTime := t.Add(3 * time.Hour)
 
 	duration := fmt.Sprintf("%d %d * * *", baghdadTime.Minute(), baghdadTime.Hour())
 
