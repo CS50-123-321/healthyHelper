c8428e664a28be9c7edc140ce7bd592695cccb52
Muntadhar Abbas[alias] <muntadhar.abbas@baly.iq>
2024-09-24 09:13:40 +0300

    changing cron

--- crons/cron.go
+++ crons/cron.go
@@ -9,8 +9,8 @@ import (
 
 func InitCron() {
 	//every("*/1 * * * *", func() { bot.Remind("Minute Reminder!!!") }) // Runs every minute for testing.
-	every("15 5 * * *", func() { bot.Remind("Let's keep our streak on!!||صباح الخير مو تنسون الرياضة||") }) // 8:15 AM Baghdad time
-	every("15 10 * * *", func() { bot.Act("bestStreak") })                                                  // 8:15 AM Baghdad time
+	every("16 16 * * *", func() { bot.Remind("Let's keep our streak on!!||صباح الخير مو تنسون الرياضة||") }) // 8:15 AM Baghdad time
+	every("16 17 * * *", func() { bot.Act("bestStreak") })                                                  // 8:20 AM Baghdad time
 	every("0 16 * * *", func() { bot.Act("SendStatus") })                                                   // 7:00 PM Baghdad time
 	every("0 18 * * *", func() { bot.Act("dailyWatch") })                                                   // 10:00 PM Baghdad time
 }
