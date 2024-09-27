89be286ce7f9ef3d480c297f4efe3dbfdc29efea
Muntadhar Abbas[alias] <muntadhar.abbas@baly.iq>
2024-09-27 18:10:50 +0300

    adding more crons to get ai msg

--- crons/cron.go
+++ crons/cron.go
@@ -15,6 +15,12 @@ func InitCron() {
 	every12("08:20AM", func() { bot.Act(bot.BestStreakUseCASE) })             // 08:20 AM Baghdad Time
 	//every12("07:00PM", func() { bot.Acbot.t("SendStatus") })                // 07:00 PM Baghdad Time
 	every12("10:00PM", func() { bot.Act(bot.DailyWatchUseCASE) }) // 10:00 PM Baghdad Time
+
+	every12("06:30PM", func() { bot.Act(bot.GenerateAiRandomMemberUseCASE) }) // 08:15 AM Baghdad Time
+	every12("07:00PM", func() { bot.Act(bot.GenerateAiRandomMemberUseCASE) }) // 08:15 AM Baghdad Time
+	every12("08:00PM", func() { bot.Act(bot.GenerateAiRandomMemberUseCASE) }) // 08:15 AM Baghdad Time
+	every12("09:00PM", func() { bot.Act(bot.GenerateAiRandomMemberUseCASE) }) // 08:15 AM Baghdad Time
+	every12("10:00PM", func() { bot.Act(bot.GenerateAiRandomMemberUseCASE) }) // 08:15 AM Baghdad Time
 }
 
 func every12(twelveHourTime string, job func()) {
